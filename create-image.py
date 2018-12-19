576380b387065048e62551364495d3d2d094d243
Zhuo Zhen <zhenz@uchicago.edu>
2018-12-19 10:38:40 -0600

    exit if running kernel doesn't match chroot kernel; clean up code

--- create-image.py
+++ create-image.py
@@ -12,34 +12,11 @@ import operator
 import os
 import sys
 
-import requests
-
-PATH = 'https://cloud.centos.org/centos/7/images/'
-INDEX = PATH + 'image-index'
-
-LATEST = 'latest'
-
-
-def image_index():
-    index = requests.get(INDEX)
-    filelike = io.StringIO(index.text)
-    cp = configparser.ConfigParser()
-    cp.readfp(filelike)
-    data = {sec: dict(cp.items(sec)) for sec in cp.sections()}
-    for sec in data:
-        data[sec]['url'] = PATH + data[sec]['file']
-    return data
-
-
-def newest_image():
-    return max(image_index().values(), key=operator.itemgetter('revision'))
-
-
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument('-r', '--revision', type=str, default=LATEST,
-        help='Revision to build with, usually of the format YYMM')
+    parser.add_argument('-r', '--revision', type=str,
+        help='Revision to build with, usually of the format YYMM', required=True)
     parser.add_argument('-v', '--variant', type=str,
         help='Image variant to build.') # extra elements defined in the .sh
     parser.add_argument('-c', '--cuda-version', type=str, default='cuda9',
@@ -49,14 +26,11 @@ def main():
 
     args = parser.parse_args()
 
-    if args.revision == LATEST:
-        image = newest_image()
-    else:
-        try:
-            image = next(i for i in image_index().values() if i['revision'] == args.revision)
-        except StopIteration:
-            print("No image found for revision '{}'".format(args.revision))
-            return 1
+    try:
+        image = next(i for i in image_index().values() if i['revision'] == args.revision)
+    except StopIteration:
+        print("No image found for revision '{}'".format(args.revision))
+        return 1
 
     # os.environ['IMAGE_URL'] = image['url']
     os.environ['BASE_IMAGE_XZ'] = image['file']
